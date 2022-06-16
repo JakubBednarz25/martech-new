83ecb339995f7937b62ac799411f4f6be12e4de1
JakubBednarz25 <73561350+JakubBednarz25@users.noreply.github.com>
2022-06-16 20:59:10 +0100

    Added Rubena certificate

--- pages/certificates.js
+++ pages/certificates.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+import Image from "next/image";
+
+import SimpleReactLightbox from "simple-react-lightbox";
+import { SRLWrapper } from "simple-react-lightbox";
+
 import styles from "../styles/Certificates.module.scss";
 
 const certificates = () => {
@@ -7,12 +12,30 @@ const certificates = () => {
     <div className={styles.certificatesPage}>
       <h1>Certificates</h1>
       <hr />
-      <div className={styles.certificatesContainer}>
-        <div className={styles.cert}>
-          <img src={"/cert1.PNG"} />
-          <h2>Pimot EN</h2>
-        </div>
-      </div>
+      <SimpleReactLightbox>
+        <SRLWrapper>
+          <div className={styles.certificatesContainer}>
+            <a href="/cert1.PNG" className={styles.cert}>
+              <Image
+                src={"/cert1.PNG"}
+                alt="Pimot EN Certificate"
+                width={200}
+                height={300}
+              />
+              <h2>Pimot EN</h2>
+            </a>
+            <a href="/cert1.PNG" className={styles.cert}>
+              <Image
+                src={"/cert2.PNG"}
+                alt="Rubena Certificate"
+                width={200}
+                height={300}
+              />
+              <h2>Rubena</h2>
+            </a>
+          </div>
+        </SRLWrapper>
+      </SimpleReactLightbox>
     </div>
   );
 };
